0e51301cb3bf9597eec836b4f11a2f92ce4b3f41
mroushdy <marwanfroushdy@gmail.com>
2013-06-03 14:22:03 +0200

    Initial Commit

---
+++ Machismo/CardGameAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CardGameAppDelegate.h
+//  Machismo
+//
+//  Created by Marwan on 6/3/13.
+//  Copyright (c) 2013 Marwan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Machismo/CardGameViewController.h
@@ -0,0 +1,13 @@
+//
+//  CardGameViewController.h
+//  Machismo
+//
+//  Created by Marwan on 6/3/13.
+//  Copyright (c) 2013 Marwan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameViewController : UIViewController
+
+@end
